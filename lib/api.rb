527f14b32e97ca2d41b4cda82c28ad190ef18ac9
Cory <cmarker4@gmail.com>
2020-12-12 19:38:58 -0600

    Added api file

---
+++ lib/api.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+require 'awesome_print'
+
+require 'net/http'
+require 'json'
+
+class DogsAPI
+  attr_reader :url
+
+  def initialize
+    @url = 'https://api.thedogapi.com/v1/breeds'
+  end
+
+
+
+
+
+
+  
+
+  def get_dog_breeds_url
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    data = JSON.parse(response)
+  end
+
+end
+
+DogsAPI.new.get_dog_breeds_url
