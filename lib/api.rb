c3419ccc33cc2fcacc063cc51477bf643463baeb
Cory <cmarker4@gmail.com>
2020-12-12 22:42:52 -0600

    Pry testing

--- lib/api.rb
+++ lib/api.rb
@@ -1,7 +1,4 @@
-require 'net/http'
-require 'json'
-
-class DogsAPI
+class DogBreed::API
   attr_reader :url
 
   def initialize
@@ -9,10 +6,12 @@ class DogsAPI
   end
 
   def get_dog_breeds_url
+    puts 'Loading dataset.'
     uri = URI(url)
     response = Net::HTTP.get(uri)
     data = JSON.parse(response)
-    puts 'This ran!'
+    puts 'Dataset loaded!'
+    binding.pry
   end
 
 end
