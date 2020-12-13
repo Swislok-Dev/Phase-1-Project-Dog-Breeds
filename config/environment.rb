ded8f5e8ac52ca7c72339731be431c544e74ee03
Cory <cmarker4@gmail.com>
2020-12-12 23:09:38 -0600

    Clean up

--- config/environment.rb
+++ config/environment.rb
@@ -8,11 +8,7 @@ require 'awesome_print'
 require_all 'lib/modules'
 require_all 'lib'
 
-
 # require_relative '../lib/cli.rb'
 # require_relative '../lib/api.rb'
 # require_relative '../lib/dog.rb'
 # require_relative '../lib/modules/dog_breed.rb'
-
-# DogsAPI.new.get_dog_breeds_url
-
