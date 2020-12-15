f90e796900f1860d15f7df0202c9ee2c627bb7f7
Cory <cmarker4@gmail.com>
2020-12-14 18:17:18 -0600

    rename dog_breeds.rb to dog_breed.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,9 +6,9 @@ require 'pry'
 
 require 'awesome_print'
 
-require_relative '../lib/modules/dog_breed.rb'    # Used for namespacing
-require_all 'lib'                                 # Requires all files to function
+require_relative '../lib/modules/dog_breeds.rb'    # Used for namespacing
+# require_all 'lib'                                 # Requires all files to function
 
-# require_relative '../lib/cli.rb'
-# require_relative '../lib/api.rb'
-# require_relative '../lib/dog.rb'
+require_relative '../lib/cli.rb'
+require_relative '../lib/api.rb'
+require_relative '../lib/dog_breed.rb'
