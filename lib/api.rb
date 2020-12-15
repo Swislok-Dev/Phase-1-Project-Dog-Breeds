52bad737c6a787c504b5df9b2cd4b4b3af989cf5
Cory <cmarker4@gmail.com>
2020-12-14 20:28:22 -0600

    Removed variable assignment to Breed class

--- lib/api.rb
+++ lib/api.rb
@@ -13,34 +13,7 @@ class DogBreeds::API
     data.each { |dog| DogBreeds::Breed.new(DogBreeds.remove_elements(dog)) }
   end
 
-  # def remove_elements(dataset)
-  #   new_hash = {}
-  #   dataset.each do |key, value|
-  #     next if key == "image" || key == "reference_image_id"
-  #     new_hash[key] << value
-  #   end
-  #   new_hash
-  # end
-
-  def dww
-    data = get_dog_breeds
-    new_hash = {
-      "weight" => get_dog_breeds[0]["weight"]["imperial"],
-      "height" => get_dog_breeds[1]["height"]["imperial"],
-      "id" => get_dog_breeds[2]["id"],
-    }
-    binding.pry
-    DogBreeds::Breed.new(new_hash)
-  end
-
-
-  # def get_breed_data(url)
-  #   uri = URI(url)
-  #   response - Net::HTTP.get(uri)
-  #   data = JSON.parse(response)
-  #   DogBreeds::Breed.new(data)
-  # end
-
+ 
 end
 
 
