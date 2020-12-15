690d87d06138b664c0d8f764cba23476672cceb8
Cory <cmarker4@gmail.com>
2020-12-14 18:23:49 -0600

    Hash method added

--- lib/api.rb
+++ lib/api.rb
@@ -1,19 +1,54 @@
 class DogBreeds::API
-  attr_reader :url
-
+  attr_accessor :breed
   def initialize
     @url = 'https://api.thedogapi.com/v1/breeds'
   end
 
   def get_dog_breeds
     puts 'Loading dataset.'
-    uri = URI(url)
+    uri = URI(@url)
     response = Net::HTTP.get(uri)
     data = JSON.parse(response)
-    puts 'Dataset loaded!'
-    binding.pry
+    data
+
+   # data.each do |dog|
+    #  DogBreeds::Breed.new(dog)
+    #end
+
+    # data.collect { |dog| dog = DogBreeds::Breed.new(dog) }
+
+    # data.each do |dog|
+    #   get_breed_data(dog)
+    # end
+   # puts 'Dataset loaded!'
+    #binding.pry
   end
 
+  def data_we_want
+    data = get_dog_breeds
+    data.each 
+    new_hash = {
+      "weight" => get_dog_breeds[0]["weight"]["imperial"],
+      "height" => get_dog_breeds[1]["height"]["imperial"],
+      "id" => get_dog_breeds[2]["id"],
+    }
+    binding.pry
+    DogBreeds::Breed.new(new_hash)
+  end
+   # binding.pry
+  binding.pry
+
+
+  
+
+  # def get_breed_data(url)
+  #   uri = URI(url)
+  #   response - Net::HTTP.get(uri)
+  #   data = JSON.parse(response)
+  #   DogBreeds::Breed.new(data)
+  # end
+
 end
+
 
 
