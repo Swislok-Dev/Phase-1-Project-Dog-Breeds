1205bdf5aae4224d9004b0be48e2e66ee3b0eb7c
Cory <cmarker4@gmail.com>
2020-12-14 20:09:08 -0600

    Clean up

--- lib/breed.rb
+++ lib/breed.rb
@@ -4,22 +4,15 @@ class DogBreeds::Breed
   @@all = []
 
   def initialize(data)
-    @breeds = []
     @breed = self
     data.each do |key, value|
       self.class.attr_accessor key
       self.send("#{key}=", value)
     end
     @@all << self
-    @breeds << self
-    binding.pry
   end 
 
   def self.all
     @@all
-  end
-
-  def breeds
-    @breeds
   end
 end
