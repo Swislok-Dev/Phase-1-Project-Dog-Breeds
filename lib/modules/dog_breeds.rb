a504ef801d3a4befcfd2750cf6d8c56c07c76e81
Cory <cmarker4@gmail.com>
2020-12-16 08:24:15 -0600

    Add add_attributes method added to be called anywhere

--- lib/modules/dog_breeds.rb
+++ lib/modules/dog_breeds.rb
@@ -1,9 +1,15 @@
 module DogBreeds
+  def self.add_attributes(dataset)
+    dataset.map do |key, value|
+      self.class.attr_accessor key
+      self.send("#{key}=", value)
+    end
+  end
+
   def self.remove_elements(dataset)
     new_hash = {}
     dataset.each do |key, value|
       next if key == "image" || key == "reference_image_id"
-
       new_hash[key] = value
     end
     new_hash
