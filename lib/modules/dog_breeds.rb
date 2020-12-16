768bb17004103df2b36483d9cab46fd77b253fce
Cory <cmarker4@gmail.com>
2020-12-16 12:55:45 -0600

    Remove unused methods

--- lib/modules/dog_breeds.rb
+++ lib/modules/dog_breeds.rb
@@ -14,43 +14,4 @@ module DogBreeds
     end
     new_hash
   end
-
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-#   def list_data(array)
-
-#   end
-  
-#   def save
-#     self.class.all << self
-#   end
-
-#   def self.find_by_id(id)
-#   end
-# end
-
-
-
-# The follow code may or may not be used.
-  # def dww
-  #   data = get_dog_breeds
-  #   new_hash = {
-  #     "weight" => get_dog_breeds[0]["weight"]["imperial"],
-  #     "height" => get_dog_breeds[1]["height"]["imperial"],
-  #     "id" => get_dog_breeds[2]["id"],
-  #   }
-  #   binding.pry
-  #   DogBreeds::Breed.new(new_hash)
-  # end
