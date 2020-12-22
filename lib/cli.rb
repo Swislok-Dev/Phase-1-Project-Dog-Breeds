897a83e892f8fe8efc9150f013650e1caeb54215
cmarker <cmarker4@gmail.com>
2020-12-21 21:52:50 -0600

    Add comments

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,6 +5,7 @@ class DogBreeds::CLI
     welcome
   end
 
+  # Give short welcome message and begins by running the API and displaying the breeds in a long list
   def welcome
     puts "Hello, and welcome to this quick reference to various dog breeds."
     puts "Select a number to view more of that breed.\n".blue
@@ -13,6 +14,7 @@ class DogBreeds::CLI
     select_breed
   end
 
+  # Uses a get request for API to load local variable parsed to json format
   def get_data
     breeds_array = DogBreeds::API.new.get_dog_breeds
     breeds_array.compact
@@ -21,6 +23,7 @@ class DogBreeds::CLI
     end
   end
 
+  # Method to display entire list of breeds from API
   def list_breeds
     puts "\n"
     list_position = 1
@@ -32,10 +35,14 @@ class DogBreeds::CLI
     end  
   end
 
+  # Takes input from user to display more information
   def select_breed
     puts "\nSelect a number to view more of that breed.\n".blue
     input = gets.strip.to_i 
-    puts "\n"                                                                # Create error for non-number
+    puts "\n"                                                                
+    
+    # Create error for non-number
+    # Will replay method to display list
     if input < 1 || input > DogBreeds::Breed.all.count
       system('clear')
       list_breeds
@@ -43,6 +50,7 @@ class DogBreeds::CLI
       select_breed 
     end
     
+    # Creates variable to search for selected breed and displays all available information
     selected_breed = DogBreeds::Breed.all[input - 1]
     puts "Breed: #{selected_breed.name}".green if selected_breed.name
     puts "Breed group: #{selected_breed.breed_group}".green if selected_breed.breed_group
@@ -58,6 +66,7 @@ class DogBreeds::CLI
     run_again?
   end
 
+  # Method to query a new object or allows user to quit out of the CLI application
   def run_again?
     puts 'Would you like to select a different breed?'
     puts 'Y/N'
@@ -76,6 +85,7 @@ class DogBreeds::CLI
     end
   end
 
+  # Displays goodbye message
   def goodbye
     puts "Thank you and"
     sleep(0.3)
